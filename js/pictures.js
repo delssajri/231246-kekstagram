7f3bd15cc0d60b1e6edd6f57133db48e95d42398
Tanya Drozdova <delssajri@gmail.com>
2017-08-27 01:10:51 +0300

    show picture on click and keydown

--- js/pictures.js
+++ js/pictures.js
@@ -38,6 +38,16 @@ var generateTestFotos = function (fotosCount, sampleComments) {
   return setObjects;
 };
 
+var mkOnClickCallback = function(link, foto) {
+  return function(event) {
+      link.href = '#';
+      event.stopPropagation();
+
+      showPicture(foto);
+  };
+};
+
+// Заполняем галлерею фотографиями, используя template
 var generateGalleryFoto = function (foto) {
   var template = document.querySelector('#picture-template').content;
   var fragment = document.createDocumentFragment();
@@ -49,21 +59,43 @@ var generateGalleryFoto = function (foto) {
     elem.setAttribute('src', foto[i].url);
     elementFoto.querySelector('.picture-comments').textContent = foto[i].comments;
     elementFoto.querySelector('.picture-likes').textContent = foto[i].likes;
+
+    var link = elementFoto.querySelector('a')
+    link.addEventListener('click', mkOnClickCallback(link, foto[i]));
+
     fragment.appendChild(elementFoto);
   }
-
   galleryPictures.appendChild(fragment);
+
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 27) {
+      closePicture();
+    }
+  });
+
+  var closeElement = document.querySelector('.gallery-overlay-close');
+  closeElement.addEventListener('click', function() {
+    closePicture();
+  });
+
+  closeElement.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) {
+      closePicture();
+    }
+  });
 };
 
+// Добавляем в картинку url, количество комментарий и лайков
 var configurePictureView = function (foto) {
   var galleryElement = document.querySelector('.gallery-overlay');
   var galleryImg = galleryElement.querySelector('img');
 
   galleryImg.setAttribute('src', foto.url);
-  galleryElement.querySelector('.comments-count').textContent = foto.comments;
+  galleryElement.querySelector('.comments-count').textContent = foto.comments.length;
   galleryElement.querySelector('.likes-count').textContent = foto.likes;
 };
 
+// Функция показа фотографии
 var showPicture = function (foto) {
   configurePictureView(foto);
 
@@ -71,11 +103,10 @@ var showPicture = function (foto) {
   galleryElement.classList.remove('hidden');
 };
 
-// var hidePicture = function () {
-//   var galleryElement = document.querySelector('.gallery-overlay');
-//   galleryElement.classList.add('hidden');
-// };
+var closePicture = function () {
+  var galleryElement = document.querySelector('.gallery-overlay');
+  galleryElement.classList.add('hidden');
+};
 
 var fotos = generateTestFotos(setObjectLength, setСomments);
 generateGalleryFoto(fotos);
-showPicture(fotos[0]);
